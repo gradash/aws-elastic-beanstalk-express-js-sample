de6289ec600a5e45492cc6bb7e498c12b3e7ec52
gradash <gradash@gmail.com>
2021-04-27 12:48:04 +0300

    Update app.js
    review test in aws

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Yoba!'));
+app.get('/', (req, res) => res.send('Hello World Yoba with REVIEW NOW!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
